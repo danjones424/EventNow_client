d289a4288828d0fddb86d5a1a70a540c238cced8
Sam Bencivengo <sambencivengo@gmail.com>
2021-12-08 12:26:27 -0500

    Proxy fixed

--- eventnow_client/src/components/NewLogin.js
+++ eventnow_client/src/components/NewLogin.js
@@ -1,49 +1,87 @@
-import React, {useState}from 'react'
-import {Link} from 'react-router-dom'
-import { Grid,Paper, Avatar, TextField, Button, Typography } from '@material-ui/core'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import {
+	Grid,
+	Paper,
+	Avatar,
+	TextField,
+	Button,
+	Typography,
+} from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 
-import axios from 'axios'
+import axios from 'axios';
 
-const Login=()=>{
+const Login = () => {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
 
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		axios
+			.post('/login', null, {
+				params: {
+					username,
+					password,
+				},
+			})
+			.then((resp) => {
+				console.log(resp);
+			})
+			.catch((error) => {
+				console.log('error', error);
+			});
+	};
 
-    const handleSubmit = e => {
-        e.preventDefault()
-        axios.post("http://localhost:3000/login", null, { params: {
-            username,
-            password}})
-        .then(resp => {
-          console.log(resp)
-        })
-        .catch(error => {
-          console.log("error", error);
-        })}
+	const paperStyle = {
+		padding: 20,
+		height: '70vh',
+		width: '50vh',
+		margin: '20px auto',
+	};
+	const avatarStyle = { backgroundColor: '#CC5A71' };
+	const btnstyle = { margin: '8px 0', backgroundColor: '#F0F757' };
 
-    const paperStyle={padding :20,height:'70vh',width:'50vh', margin:"20px auto"}
-    const avatarStyle={backgroundColor:'#CC5A71'}
-    const btnstyle={margin:'8px 0', backgroundColor: '#F0F757'}
-    
-    
-    return(
-        <Grid>
-            <Paper elevation={10} style={paperStyle}>
-                <Grid align='center'>
-                     <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar>
-                    <h2>Sign In</h2>
-                </Grid>
-                <TextField label='Username' placeholder='Enter username' onChange={(e) => setUsername(e.target.value)}fullWidth required/>
-                <TextField label='Password' placeholder='Enter password' type='password' onChange={(e) => setPassword(e.target.value)} fullWidth required/>
-                <Button type='submit'  onClick={handleSubmit} variant="contained" style={btnstyle} fullWidth>Sign in</Button>
-                <Typography class="typo"> 
-                    Do you have an account? 
-                    <Link to="/signup"> Sign Up </Link>
-                </Typography>
-            </Paper>
-        </Grid>
-    )
-}
+	return (
+		<Grid>
+			<Paper elevation={10} style={paperStyle}>
+				<Grid align="center">
+					<Avatar style={avatarStyle}>
+						<LockOutlinedIcon />
+					</Avatar>
+					<h2>Sign In</h2>
+				</Grid>
+				<TextField
+					label="Username"
+					placeholder="Enter username"
+					onChange={(e) => setUsername(e.target.value)}
+					fullWidth
+					required
+				/>
+				<TextField
+					label="Password"
+					placeholder="Enter password"
+					type="password"
+					onChange={(e) => setPassword(e.target.value)}
+					fullWidth
+					required
+				/>
+				<Button
+					type="submit"
+					onClick={handleSubmit}
+					variant="contained"
+					style={btnstyle}
+					fullWidth
+				>
+					Sign in
+				</Button>
+				<Typography className="typo">
+					Do you have an account?
+					<Link to="/signup"> Sign Up </Link>
+				</Typography>
+			</Paper>
+		</Grid>
+	);
+};
 
-export default Login
+export default Login;
