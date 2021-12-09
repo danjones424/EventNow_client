bd70516c0c66767ff3f68589b965b407b6ac2798
Sam Bencivengo <sambencivengo@gmail.com>
2021-12-09 12:10:29 -0500

    RSVP button created

--- eventnow_client/src/components/AllEventsContainer.js
+++ eventnow_client/src/components/AllEventsContainer.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { useEffect, useState } from 'react';
 import EventItem from './EventItem';
 
@@ -18,9 +19,54 @@ const AllEventsContainer = ({ currentUser }) => {
 
 	const renderEvent = (eventsToRender) => {
 		return eventsToRender.map((eventItem) => {
-			return <EventItem key={eventItem.id} eventItem={eventItem} />;
+			return (
+				<EventItem
+					handleRSVP={handleRSVP}
+					key={eventItem.id}
+					eventItem={eventItem}
+				/>
+			);
 		});
 	};
+
+	const handleRSVP = (e) => {
+		const eventID = e.id;
+		const userID = currentUser;
+
+		fetch('/attendances', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				user_id: userID,
+				event_id: eventID,
+				host: false,
+			}),
+		})
+			.then((r) => r.json())
+			.then((attendance) => console.log('Attendance Creaed!'))
+			.catch((error) => {
+				console.error('Error:', error);
+			});
+	};
+
+	// axios
+	// 		.post('/login', null, {
+	// 			params: {
+	// 				username,
+	// 				password,
+	// 			},
+	// 		})
+	// 		.then((resp) => {
+	// 			console.log(resp);
+	// 			setAlert(false);
+	// 			handleLogin();
+	// 		})
+	// 		.catch((error) => {
+	// 			console.log('error', error);
+	// 			setAlert(true);
+	// 		});
 
 	return <>{contentLoaded ? renderEvent(fetchedEvents) : null}</>;
 };
--- eventnow_client/src/components/EventItem.js
+++ eventnow_client/src/components/EventItem.js
@@ -8,13 +8,25 @@ const EventItem = ({ eventItem, handleRSVP }) => {
 		margin: '20px auto',
 	};
 
+	const btnstyle = {
+		margin: '8px 0',
+		color: '#34344A',
+		backgroundColor: '#F0F757',
+	};
+
 	return (
 		<div>
 			<Paper elevation={10} style={paperStyle}>
 				<h2>{eventItem.event_name}</h2>
 				<h3>{eventItem.category}</h3>
 				<p>{eventItem.description}</p>
-				<Button onClick={handleRSVP}>RSVP</Button>
+				<Button
+					variant="contained"
+					style={btnstyle}
+					onClick={() => handleRSVP(eventItem)}
+				>
+					RSVP
+				</Button>
 			</Paper>
 		</div>
 	);
